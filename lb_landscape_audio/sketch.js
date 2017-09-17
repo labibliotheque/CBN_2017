92289db705388ea9b67eec02388823406af9862a
Berenger <berenger.recoules@gmail.com>
2017-09-17 18:59:06 +0200

    finish-up audio landscape

--- lb_landscape_audio/sketch.js
+++ lb_landscape_audio/sketch.js
@@ -1,10 +1,4 @@
-/*
-
-Ajouter un petit son au début de chaque jour
-Ajouter un lfo dont la fréquence dépend de l'activité générale ?
-
-*/
-
+// passer à un synthétiseur polyphonique ou soundfont ?
 var mois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
 var jours = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
 /* Lieux*/
@@ -13,60 +7,64 @@ var lieux = [
 ];
 /* Emplacements */
 var emplacements = [
-    "arts","cinema adultes", "cinema jeunesse", "graphisme","danse","musique adultes","musique jeunesse","theatre","albums","bd adultes","bd jeunesse","litterature", "romans adultes","romans jeunesse","geographie",
- 	"histoire",
- 	"informatique",
- 	"langues",
- 	"loisirs creatifs",
- 	"philosophie",
- 	"psychologie",
- 	"sciences",
-	"fonds pro",
-    "presse",
- 	"societe",
- 	"sports et loisirs",
- 	"vie pratique",
-    "livres sur les jeux",
-    "jeux d'assemblage",
- 	"jeux d'exercices",
- 	"jeux a regles",
- 	"jeux symboliques",
-	"jeux video",
-]
+    "arts", "cinema adultes", "cinema jeunesse", "graphisme", "danse", "musique adultes", "musique jeunesse", "theatre", "albums", "bd adultes", "bd jeunesse", "litterature", "romans adultes", "romans jeunesse", "geographie"
+ 	, "histoire"
+ 	, "informatique"
+ 	, "langues"
+ 	, "loisirs creatifs"
+ 	, "philosophie"
+ 	, "psychologie"
+ 	, "sciences"
+	, "fonds pro"
+    , "presse"
+ 	, "societe"
+ 	, "sports et loisirs"
+ 	, "vie pratique"
+    , "livres sur les jeux"
+    , "jeux d'assemblage"
+ 	, "jeux d'exercices"
+ 	, "jeux a regles"
+ 	, "jeux symboliques"
+	, "jeux video"
+, ]
 var db
-//
+    //
 var fontBold
 var fontRegular
-// buffers
+    // buffers
 var buffer; // audio
 var imgBuffer, img; // graphic
-//gui
+var yoffset = -2.0
+    //gui
 var settings
-// sequencing
+    // sequencing
 var click
 var myPart // un metronome
 var index = 1 // la position du sequenceur
-var playbackS = 60 // la vitesse de défilement
+var beat = 0;
+var playbackS = 90 // la vitesse de défilement
 var stopIndex = 31;
 var loop = true;
 var play = true;
-var interSpeed = 0.85;
-
+var interSpeed = 0.35;
+var totPlay = true;
 
 function preload() {
     db = loadJSON("../data/db_06122017.json");
     fontBold = loadFont("../assets/RenneBolArcTyp.otf")
     fontRegular = loadFont("../assets/RenneArcTyp.otf")
     ctx = getAudioContext();
-    bufferSize = 1024
+    // buffer de 256 car il y a 33 emplacement fois 6 lieux = 198 on va donc padder la fin du buffer avec des zeros
+    // qu'on ne represente pas
+    bufferSize = 256
     node = ctx.createBufferSource();
     buffer = ctx.createBuffer(1, bufferSize, ctx.sampleRate);
     data = buffer.getChannelData(0);
     for (var i = 0; i < bufferSize; i++) {
         data[i] = 0;
     }
-
     click = loadSound("../assets/134062__allibrante__allibrante-impulse-waveform-1.wav");
+    console.log(emplacements.length, lieux.length, emplacements.length * lieux.length)
 }
 
 function setup() {
@@ -77,53 +75,61 @@ function setup() {
     textAlign(LEFT, TOP);
     textFont(fontBold);
     textSize(25);
-    //
+    // audio buffer to fill with data
     node.buffer = buffer;
     node.loop = true;
     node.connect(p5.soundOut);
     node.start(0);
-    //
+    // synth
+    osc = new p5.SinOsc();
+    osc.amp(0);
+    osc.start();
+    amplitude = new p5.Amplitude();
+    // sequencer
     myPart = new p5.Part();
-    var pulse = new p5.Phrase('pulse', step, [1, 1, 1, 1]);
+    var pulse = new p5.Phrase('pulse', step, [1, 1, 1, 1, 1]);
     myPart.addPhrase(pulse); // on ajoute notre phrase à l'objet part
     myPart.setBPM(playbackS);
     myPart.start();
     myPart.loop();
-    //
+    // graphical buffers
     img = createGraphics(windowWidth, windowHeight);
     imgBuffer = createGraphics(windowWidth, windowHeight);
-    imgBuffer.background(0);
+    //imgBuffer.background(0);
     img.background(0);
     imgBuffer.colorMode(HSB, 360, 100, 100);
+    imgBuffer.background(200,100,20);
+    //img.background(200,100,20);
+    // moving title
     title = new Title();
-
-
+    // gui window
     settings = QuickSettings.create(windowWidth - 475, 25, "Informations et parmètres");
+    settings.collapse();
     settings.setWidth(450);
-    settings.addHTML("Informations", "<p>Cette page web permet de sonifier les données issues des statistiques de prêts. Les données sont interprétés comme des signaux audio (valeurs entre -1 et 1) et sont directement injectées dans la carte son de l'ordinateur. Chaque jour dispose alors d'un son qui lui est propre généré directement à partir des statistiques d'emprunts des usagers.");
+    settings.addHTML("Informations", "<p>Cette page web permet de sonifier les données issues des statistiques de prêts. Les données sont interprétés comme des signaux audio (valeurs entre -1 et 1) et sont directement injectées dans la carte son de l'ordinateur.</p><p> Chaque jour dispose alors d'un son qui lui est propre généré directement à partir des statistiques d'emprunts des usagers.</p><p>Les jours sont marqués par un léger click et matérialisés par une barre blanche verticale, ils représentent des mesures musicales </p><p>Finalement, entre chaque 'pulsation' journalière, sont donnés à entendre les statistiques de totaux d'emprunt de chaque lieux pour la journée : plus le son est grave moins il y a eu d'emprunts.");
     settings.addHTML("Sélectionner les lieux à traiter", "");
-    settings.addBoolean("Bourg", false, callbackLieu);
-    settings.addBoolean("Haute Chaussée", false, callbackLieu);
+    settings.addBoolean("Bourg", true, callbackLieu);
+    settings.addBoolean("Haute Chaussée", true, callbackLieu);
     settings.addBoolean("Charles-Gautier-Hermeland", true, callbackLieu);
-    settings.addBoolean("Bellevue", false, callbackLieu);
-    settings.addBoolean("Ludothèque Municipale", false, callbackLieu);
-    settings.addBoolean("Gao Xingjian - Sillon", false, callbackLieu);
+    settings.addBoolean("Bellevue", true, callbackLieu);
+    settings.addBoolean("Ludothèque Municipale", true, callbackLieu);
+    settings.addBoolean("Gao Xingjian - Sillon", true, callbackLieu);
+    settings.addBoolean("Totaux d'emprunts quotidien", true, callbackTotal);
     settings.addHTML("Sélectionner les dates concernées", "");
     settings.addDate("Date de début", "2017-01-01", selectDateB);
     settings.addDate("Date de fin", "2017-01-31", selectDateE);
     settings.addBoolean("Boucler", true, loopCallback);
     settings.addHTML("Paramètres de Défilement", "");
     settings.addBoolean("Jouer", true, playBack);
-    settings.addRange("Vitesse", 10, 90, 60, 1, playbackSpeed);
-    settings.addRange("Vitesse d'interpolation", 0.05, 0.9, 0.80, 0.05, interpolationSpeed);
-
+    settings.addRange("Vitesse", 60, 160, 90, 1, playbackSpeed);
+    settings.addRange("Vitesse d'interpolation", 0.01, 1.0, 0.351, 0.025, interpolationSpeed);
+    settings.addRange("Perspective", -10, -0.25, -2.0, 0.05, callbackPerspective);
 }
 
 function draw() {
     //background(0);
     noStroke()
     image(img, 0, 0, windowWidth, windowHeight);
-
     if (index > stopIndex) {
         if (loop) {
             index = 1;
@@ -133,23 +139,44 @@ function draw() {
             index = 1;
         }
     }
-
-
     if (play) update();
-
     for (var i = 0; i < emplacements.length; i++) {
         push();
         textSize(18);
-        textAlign(RIGHT,TOP);
+        textAlign(RIGHT, TOP);
         var xpos = map(i, 0, emplacements.length, 0, windowWidth * 2 / 3);
-        translate(xpos, windowHeight-158);
+        translate(xpos, windowHeight - 158);
         rotate(-PI / 2);
-        fill(map(i, 0, emplacements.length, 0, 320), 100, 100)
+        fill(map(i, 0, emplacements.length, 0, 320), 60, 100)
         text(emplacements[i], 0, 0);
-         pop();
-
+        pop();
     }
-
+    if (totPlay) {
+        push();
+        noStroke();
+        textAlign(CENTER);
+        imgBuffer.push()
+        imgBuffer.translate(75 + beat * (windowWidth * 2 / 3) / lieux.length, height / 2);
+        var value = int(db[index][lieux[beat]].total);
+        var a = map(value, 0, int(db[0]["Max_Tous"]), 0.25, 1)
+        var s = map(value, 0, int(db[0]["Max_Tous"]), 0, 50)
+        imgBuffer.strokeWeight(1);
+        imgBuffer.stroke(255, a);
+        imgBuffer.noFill();
+        var lvl = amplitude.getLevel()
+        imgBuffer.ellipse(0, 0, s*(lvl+0.1), s*(lvl+0.1));
+        imgBuffer.pop();
+        pop();
+        for (var i = 0; i < lieux.length; i++) {
+            push()
+            textAlign(CENTER, CENTER);
+            translate(75 + i * (windowWidth * 2 / 3) / lieux.length, height / 2);
+            fill(150);
+            textSize(14);
+            text(lieux[i], 0, 0);
+            pop();
+        }
+    }
     push()
     textAlign(LEFT, BOTTOM);
     fill(255);
@@ -165,24 +192,41 @@ function draw() {
 }
 
 function step() {
-
-    if (play){
-
-        var rate = map(db[index].Tous.total, 0, int(db[0]["Max_Tous"]),32,52)
-        click.play(0,rate,0.25);
-        index += 1;
-
-        imgBuffer.noStroke();
-        imgBuffer.fill(0,0.05);
-        imgBuffer.rect(0,0,imgBuffer.width*2/3,windowHeight -160);
-        imgBuffer.stroke(255,0.5);
-        imgBuffer.strokeWeight(5);
-        imgBuffer.line(imgBuffer.width*2/3,windowHeight/2,imgBuffer.width*2/3,windowHeight-160);
-
-
+    if (play) {
+        beat += 1;
+        beat = beat % lieux.length
+        var value = int(db[index][lieux[beat]].total);
+        if (value != 0) {
+            if (totPlay) {
+                var midi = (map(value, 0, int(db[0]["Max_Tous"]), 36, 48))
+                var g = constrain(map(value, 0, int(db[0]["Max_Tous"]), 1, 0.21), 0.25, 1)
+                osc.freq(midiToFreq(midi));
+                osc.amp(g, 0.5);
+                osc.pan(map(beat, 0, lieux.length, -1.0, 1.0), 0.5);
+            }
+        }
+        if (beat % lieux.length == 0) {
+            click.pan(0);
+            click.play(0, 15, 1.0);
+            index += 1;
+            //imgBuffer.stroke(255);
+            imgBuffer.noStroke();
+            imgBuffer.fill(200,100,20, 0.015);
+            imgBuffer.rect(0, 0, imgBuffer.width * 2 / 3, windowHeight - 160);
+            imgBuffer.stroke(255, 0.5);
+            imgBuffer.line(0, windowHeight / 2, 0, windowHeight - 160);
+            imgBuffer.strokeWeight(5);
+            imgBuffer.line(imgBuffer.width * 2 / 3, windowHeight / 2, imgBuffer.width * 2 / 3, windowHeight - 160);
+        }
     }
+}
 
+function callbackPerspective(data) {
+    yoffset = data;
+}
 
+function callbackTotal(data) {
+    totPlay = data;
 }
 
 function playBack(data) {
@@ -224,41 +268,44 @@ function selectDateE() {
     var date = new Date(int(selectedStopDate[0]), int(selectedStopDate[1] - 1), int(selectedStopDate[2]));
     date = date.toLocaleDateString();
     for (var i = 1; i < Object.keys(db).length - 1; i++) {
-
         if (db[i]["date"] == date) {
             stopIndex = i
-
             break
         }
     }
 }
 
 function update() {
-    var yoffset = -1.5
     var xoffset = 4
         // on dessine notre image en fond de notre buffer (c'est le premier dessin qu'on effectue les suivants seront donc par dessus)
     imgBuffer.noStroke()
     imgBuffer.image(img, xoffset, yoffset, img.width, img.height);
     //buffer.filter(BLUR,0.48); // le blur est un peu gourmand à haute résolution
     // on le remplace par le "blur du pauvre" : un rectangle noir très transparent
-    //imgBuffer.fill(0, 0.0021);
+    imgBuffer.fill(200,100,20, 0.0095);
     //imgBuffer.rect(0, 0, img.width, img.height)
     updateAudioBuffer()
     img = imgBuffer;
 }
-
+// audio and image
 function updateAudioBuffer() {
     data = node.buffer.getChannelData(0)
     var indexLieu
     var indexEmplacement
+
     imgBuffer.strokeWeight(0.5)
 
-
+    randomSeed(index)// random seed for each day
     for (var i = 0; i < bufferSize; i++) {
-        indexLieu = int(i / 5) % lieux.length
-        indexEmplacement = int(int(i / 5) / lieux.length)
+        indexLieu = int(i / 1) % lieux.length
+        indexEmplacement = int(int(i / 1) / lieux.length)
         if (indexLieu < lieux.length && indexEmplacement < emplacements.length - 1) {
-            var t = float(map(db[index][lieux[indexLieu]][emplacements[indexEmplacement]], 0, int(db[0]["Max_Tous"]), 0, 1))
+            // we need values beetween -1 and 1, but according to the mean of values we have this will give most values on a negative side
+            // considering this mapping about the quantity of exchange the sign can be randomized check the randomSeed above
+            var t = constrain(float(map(db[index][lieux[indexLieu]][emplacements[indexEmplacement]], 0, int(db[0]["Max_Tous"]), 0, 0.9)), 0, 0.9)
+            if (random(0, 100) < 50) {
+                t = t * (-1)
+            }
             if (lieux[indexLieu] == "Bourg" && settings.getValuesAsJSON(false)["Bourg"] == true) {
                 data[i] += (t - data[i]) * interSpeed
             }
@@ -284,13 +331,21 @@ function updateAudioBuffer() {
         else {
             data[i] = 0
         }
-        var xpos = map(i, 0, bufferSize, 5, windowWidth * 2 / 3)
-        var hei = map(data[i], 0, 1, 1, windowHeight / 2)
-        imgBuffer.strokeWeight(0.25);
-        imgBuffer.stroke(255);
+        var xpos = map(i, 0, lieux.length * emplacements.length, 5, windowWidth * 2 / 3) // ie 198 on 256 bins the rest being zeroes
+        xpos = constrain(xpos, 0, windowWidth * 2 / 3)
+        var hei = abs(map((data[i]), -1, 1, -300, 300))
+        imgBuffer.strokeWeight(1);
+        //imgBuffer.stroke(255);
         //imgBuffer.noStroke();
-        imgBuffer.fill(map(indexEmplacement, 0, emplacements.length, 0, 320), 100, 100);
-        imgBuffer.rect(xpos, windowHeight - 160, windowWidth / bufferSize, -hei);
+        var h = map(indexEmplacement, 0, emplacements.length, 0, 320)
+        imgBuffer.noFill()
+        imgBuffer.stroke(h, 60, 100, 1);
+        imgBuffer.push()
+        imgBuffer.translate(xpos,windowHeight-160);
+        imgBuffer.rectMode(CENTER)
+        imgBuffer.rotate(PI/4)
+        imgBuffer.rect(0, 0, hei, hei);
+        imgBuffer.pop();
     }
 }
 
